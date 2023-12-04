fbeb4ca823a67da95aa4cff219e23ff97498b8b1
Dev Sanghvi <105737978+dev31sanghvi@users.noreply.github.com>
2023-12-04 16:57:45 +0530

    Week-1 work

---
+++ main.js
@@ -0,0 +1,14 @@
+// console.log("dev sanghvi");
+// const a=1;
+// a=2;
+// console.log(a);
+
+let firstName ="Dev";
+let age=20;
+let isMarried =false;
+
+if(isMarried==true){
+    console.log(firstName +" is  married")
+}else{
+    console.log(firstName +"is not married")
+}
